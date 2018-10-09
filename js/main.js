c9ad5eed35861e1abaaa4ab57239542809d2d12a
Salvador Chacon <chamba.chs.90@gmail.com>
2018-10-09 02:37:34 -0600

    Agregado Service Worker para ApiCache

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ $(document).ready(function() {
     	}
     });
 });
+
+if ('serviceWorker' in navigator) {
+  console.log('Service Worker Supporteed')
+  navigator.serviceWorker
+    .register('./sw.js', { scope: './' })
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+    })
+    .catch(function(err) {
+      console.log("Service Worker Failed to Register", err);
+    })
+
+} else{
+
+  console.log('Service Worker not supported');
+}
